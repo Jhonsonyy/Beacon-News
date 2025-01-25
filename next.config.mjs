f512a9e99c61ec70eff0bec4808b96b672d9b702
Jhonsonyy <134149091+Jhonsonyy@users.noreply.github.com>
2025-01-26 00:44:46 +0500

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media.cnn.com', // Allows all subdomains
+      },
+    ]
+  }
+};
+
+export default nextConfig;
