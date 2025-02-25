97f3aff0cd944670538cffe3b5a83f0f6b53b835
Jhonsonyy <134149091+Jhonsonyy@users.noreply.github.com>
2025-02-26 00:44:52 +0500

    index.js

--- pages/index.js
+++ pages/index.js
@@ -95,9 +95,6 @@ export default function Home({ ArticlesObj }) {
                 >
                 <button className="inline-flex text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded text-lg">Read More</button>
                 </Link>
-              <div className="m-auto text-white text-lg mt-10 border-white">
-                  <h3>Syed Wajahat</h3>
-                </div>
               </div>
             </div>
           </div>
